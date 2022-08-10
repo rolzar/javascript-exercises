c2b316784047552f0a9d2b16e5c7722c203962c2
rolzar <otakurolando@gmail.com>
2022-08-09 18:08:24 -0700

    Add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeat = "";
+    if(num < 0){
+        return "ERROR"
+    }
 
+    for(let i = 0; i < num; i++){
+        repeat += string 
+    }
+
+    return repeat
 };
 
 // Do not edit below this line
