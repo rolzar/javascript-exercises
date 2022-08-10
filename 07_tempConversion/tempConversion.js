1a4a23a10532db713acc0bdb8f439e3bccd435bf
rolzar <otakurolando@gmail.com>
2022-08-09 20:31:44 -0700

    Add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(tempFah) {
+  let tempCel;
+  let round;
+  tempCel = (tempFah - 32) * (5/9)
+  round = Math.round(tempCel*10)/10
+
+  return round
+
 
 };
 
-const ctof = function() {
+const ctof = function(tempCel) {
+  let tempFah;
+  let round;
+  tempFah = tempCel * (9/5) + 32
+  round = Math.round(tempFah*10)/10
 
+  return round
 };
 
 // Do not edit below this line
