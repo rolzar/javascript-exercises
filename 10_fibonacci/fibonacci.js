1c79ad4c5adf4e37f4d73639a9369869d539ba4a
rolzar <otakurolando@gmail.com>
2022-08-15 03:04:47 -0700

    add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if(a < 0) return "OOPS";
+    a = +a;
+
+    if(a <= 1){
+        return a
+    }
+    return fibonacci(a-1) + fibonacci(a-2)
 
 };
 
