83270d088acf4bddd89aaf0fb51d135a404c836b
rolzar <otakurolando@gmail.com>
2022-08-15 04:35:37 -0700

    add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(event) {
+   return event.sort((a,b) =>{
+        const older = yearsLived(a.yearOfBirth,a.yearOfDeath);
+        const younger = yearsLived(b.yearOfBirth,b.yearOfDeath);
+        return older < younger ? 1 : -1;
+    })[0];
+
+
 
 };
+
+function yearsLived(start, end){
+    if(!end){
+        end = new Date().getFullYear();
+    }
+
+    return end - start
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
